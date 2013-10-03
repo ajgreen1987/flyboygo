39f106c9a7ead1c813cae2df68d2bff2b8ac7983
AJ Green <ajgreen1987@gmail.com>
2013-10-03 14:14:27 -0400

    Initial Commit

---
+++ AirplaneBomb/GMSMyScene.h
@@ -0,0 +1,13 @@
+//
+//  GMSMyScene.h
+//  AirplaneBomb
+//
+
+//  Copyright (c) 2013 Green Mailbox Studios. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GMSMyScene : SKScene
+
+@end
